c62d1a7ba6f6d6c0f2b4890cc90dd6b208c63cfe
Ayoola Masha <30448980+Ayoolamasha@users.noreply.github.com>
2021-03-23 07:08:43 +0100

    init commit

---
+++ app/src/main/java/com/ayoolamasha/paytaxappsdg/Home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.ayoolamasha.paytaxappsdg.Home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/ayoolamasha/paytaxappsdg/SignUp/SignUpViewModel.kt
@@ -0,0 +1,7 @@
+package com.ayoolamasha.paytaxappsdg.SignUp
+
+import androidx.lifecycle.ViewModel
+
+class SignUpViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
